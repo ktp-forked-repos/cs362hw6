a227304e0c6c3ad22d435fac94fcf43acf420ff1
Kiran Tomlinson <tomlinsonk@carleton.edu>
2018-02-18 20:15:47 -0600

    Add basic assemble structure for real

--- assemble.py
+++ assemble.py
@@ -1,10 +1,46 @@
 import sys
 
+USAGE = 'python3 assemble.py reads_file k'
+
+
+def error(message)
+    print('Error: {}'.format(message))
+    print(USAGE)
+    exit(1)
+
+
+
+def get_kmers(reads, k):
+    kmers = []
+    # TODO: implement
+    return kmers
+    
 
 def assemble(reads, k):
-    pass
+    contigs = []
+    # TODO: implement
+    
+    
+    with open('conitgs.txt', 'w') as f:
+        f.write('\n'.join(contigs))
 
-def main(args)
 
+def main(args):
+    if len(args) != 2:
+        error('wrong number of arguments')
+        
+    try:
+        with open(args[0]) as f:
+            reads = f.readlines()
+    except FileNotFoundError:
+        error('no such file: {}'.format(args[0]))
+        
+    try:
+        k = int(args[1])
+    except ValueError:
+        error('k must be an integer')
+        
+    assemble(reads, k)
+    
 if __name__ == '__main__':
     main(sys.argv[1:])
